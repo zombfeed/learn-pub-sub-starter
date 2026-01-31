afb04ec93e553df51bcd17825dc31745232de4a9
zombfeed <rdgammad@gmail.com>
2026-01-31 11:20:05 -0800

    CH5:L7 nack requeue fix, update git ignore

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -37,9 +37,9 @@ func handlerMove(gs *gamelogic.GameState, publishCh *amqp.Channel) func(gamelogi
 			)
 			if err != nil {
 				fmt.Printf("error: %v", err)
-				return pubsub.NackDiscard
+				return pubsub.NackRequeue
 			}
-			return pubsub.NackRequeue
+			return pubsub.Ack
 		case gamelogic.MoveOutcomeSamePlayer:
 			return pubsub.NackDiscard
 		}
